ba769e5176bfd6368607d10991b9ff718cd734e8
Temo Jincharadze <temotemo161@gmail.com>
2021-09-25 02:23:33 +0400

    feat: Add getStringsLength

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -130,8 +130,10 @@ function getUpperCaseStrings(arr) {
  *    [ '', 'a', 'bc', 'def', 'ghij' ]  => [ 0, 1, 2, 3, 4 ]
  *    [ 'angular', 'react', 'ember' ] => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength(arr) {
+  const arrLength = [];
+  arr.filter((item) => arrLength.push(item.length));
+  return arrLength;
 }
 
 /**
