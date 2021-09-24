5e4deff7633219a7ea26a9339ede2bb700a55a66
Temo Jincharadze <temotemo161@gmail.com>
2021-09-24 05:01:44 +0400

    feat: Add getArrayOfPositives

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -65,8 +65,10 @@ function doubleArray(arr) {
  *    [-1, 2, -5, -4, 0] => [ 2 ]
  *    [] => []
  */
-function getArrayOfPositives(/* arr */) {
-  throw new Error('Not implemented');
+function getArrayOfPositives(arr) {
+  const pos = [];
+  Array.from(arr, (x) => (x > 0 ? pos.push(x) : false));
+  return pos;
 }
 
 /**
