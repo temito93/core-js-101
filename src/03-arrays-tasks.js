ca11ef7156ef16f44f7f95f65579f77bf9e3f6ed
Temo Jincharadze <temotemo161@gmail.com>
2021-09-26 02:38:59 +0400

    feat: Add getIdentityMatrix

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -444,8 +444,9 @@ function sortCitiesArray(arr) {
  *           [0,0,0,1,0],
  *           [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array(n).fill(Array(n).fill(0))
+    .map((item, index) => item.map((item2, index2) => (index === index2 ? 1 : 0)));
 }
 
 /**
