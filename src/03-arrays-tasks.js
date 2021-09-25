ea2ddaa4cf4fffcefac2882dfa6211c3a1a53ee3
Temo Jincharadze <temotemo161@gmail.com>
2021-09-25 04:55:01 +0400

    feat: Add sortDigitNamesByNumericOrder

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -321,8 +321,9 @@ function getPositivesCount(arr) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const str = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  return arr.sort((a, b) => str.indexOf(a) - str.indexOf(b));
 }
 
 /**
