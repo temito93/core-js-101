8ad9e4a048d07135176c029ae8d38349d265c4e7
Temo Jincharadze <temotemo161@gmail.com>
2021-09-25 04:22:53 +0400

    feat: Add getPositivesCount

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -304,8 +304,8 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  return arr.filter((item) => typeof item === 'number' && item > 0).length;
 }
 
 /**
