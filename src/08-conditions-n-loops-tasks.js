b5ecd89e700d179922791c17e309d9ee23f29c89
Temo Jincharadze <temotemo161@gmail.com>
2021-09-28 22:44:07 +0400

    feat: Add reverseInterger

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -245,8 +245,9 @@ function reverseString(str) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  const newNum = num.toString().split('');
+  return newNum.reverse().join('');
 }
 
 
